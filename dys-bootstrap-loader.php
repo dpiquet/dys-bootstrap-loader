e518b1d6ac6350d30f4f7712b98c7348f27efe47
PIQUET Damien <piqudam@gmail.com>
2014-11-20 21:32:44 +0100

    Wordpress coding rules

--- dys-bootstrap-loader.php
+++ dys-bootstrap-loader.php
@@ -7,12 +7,12 @@ Author: Damien PIQUET & Bootstrap Team
 Author URI: https://github.com/dpiquet/
 */
 
-if (! defined('ABSPATH')) {
+if (! defined('ABSPATH') ) {
 	die('');
 }
 
-define('DYS_BOOTSTRAP_LOADER_LATEST_VERSION', '3.3.0');
-define('DYS_BOOTSTRAP_PLUGIN_NAME', 'dys-bootstrap-loader');
+define( 'DYS_BOOTSTRAP_LOADER_LATEST_VERSION', '3.3.0' );
+define( 'DYS_BOOTSTRAP_PLUGIN_NAME', 'dys-bootstrap-loader' );
 
 /** Associative array for easy maintenance */
 $bootstrap_versions = Array(
@@ -24,9 +24,9 @@ $bootstrap_versions = Array(
 	'3.3.0'    => 'resources/bootstrap-3.3.0'
 	);
 
-function set_bootstrap_version($version) {
+function set_bootstrap_version( $version ) {
 	/** use wordpress set_option */
-	return update_option( 'dys-bootstrap-loader-status', $version);
+	return update_option( 'dys-bootstrap-loader-status', $version );
 }
 
 function get_bootstrap_version() {
@@ -37,30 +37,30 @@ function get_bootstrap_version() {
 function get_bootstrap_minified() {
 	$minified = get_option( 'dys-bootstrap-loader-use-minified' );
 
-	if ($minified == 'yes') { return true; }
+	if ( $minified == 'yes' ) { return true; }
 	else { return false; }
 }
 
 function get_bootstrap_parts() {
-	return get_option('dys-bootstrap-loader-parts');
+	return get_option( 'dys-bootstrap-loader-parts' );
 }
 
-function set_bootstrap_parts($parts) {
-	if ($parts == get_bootstrap_parts()) { return true; }
+function set_bootstrap_parts( $parts ) {
+	if ( $parts == get_bootstrap_parts() ) { return true; }
 
-	return update_option('dys-bootstrap-loader-parts', $parts);
+	return update_option( 'dys-bootstrap-loader-parts', $parts );
 }
 
 function enable_bootstrap_minified() {
-	if (get_bootstrap_minified()) { return true; }
+	if ( get_bootstrap_minified() ) { return true; }
 
-	return update_option( 'dys-bootstrap-loader-use-minified', 'yes');
+	return update_option( 'dys-bootstrap-loader-use-minified', 'yes' );
 }
 
 function disable_bootstrap_minified() {
-	if (! get_bootstrap_minified()) { return true; }
+	if (! get_bootstrap_minified() ) { return true; }
 
-	return update_option( 'dys-bootstrap-loader-use-minified', 'no');
+	return update_option( 'dys-bootstrap-loader-use-minified', 'no' );
 }
 
 function twitterbootstrap_header() {
@@ -68,17 +68,17 @@ function twitterbootstrap_header() {
 	if ( !is_admin() ) {
 		global $bootstrap_versions;
 
-		if (get_bootstrap_parts() == 'jsonly') { return; }
+		if ( get_bootstrap_parts() == 'jsonly' ) { return; }
 
 		$version = get_bootstrap_version();
 		$useMinified = get_bootstrap_minified();
 
-		if ($useMinified) { $cssFile = 'bootstrap.min.css'; }
+		if ( $useMinified ) { $cssFile = 'bootstrap.min.css'; }
 		else { $cssFile = 'bootstrap.css'; }
 
-		if ($version && ($version != 'disabled')) {
+		if ( $version && ($version != 'disabled') ) {
 
-			if ($version == 'latest')
+			if ( $version == 'latest' )
 				$version = DYS_BOOTSTRAP_LOADER_LATEST_VERSION;
 
 			wp_register_style( 'bootstrap',
@@ -95,16 +95,16 @@ function twitterbootstrap_footer() {
 	global $bootstrap_versions;
 	$version = get_bootstrap_version();
 
-	if (get_bootstrap_parts() == 'cssonly') { return; }
+	if ( get_bootstrap_parts() == 'cssonly' ) { return; }
 
-	if ($version == 'disabled')
+	if ( $version == 'disabled' )
 		return;
-	if($version == 'latest')
+	if( $version == 'latest' )
 		$version = DYS_BOOTSTRAP_LOADER_LATEST_VERSION;
 
 	$useMinified = get_bootstrap_minified();
 
-	if ($useMinified) { $jsFile = 'bootstrap.min.js'; }
+	if ( $useMinified ) { $jsFile = 'bootstrap.min.js'; }
 	else { $jsFile = 'bootstrap.js'; }
 
 	wp_enqueue_script( 'bootstrap',
@@ -114,16 +114,16 @@ function twitterbootstrap_footer() {
 
 function deactivate_bootstrap_loader() {
 	/** remove options from database */
-	delete_option('dys-bootstrap-loader-status');
-	delete_option('dys-bootstrap-loader-use-minified');
-	delete_option('dys-bootstrap-loader-parts');
+	delete_option( 'dys-bootstrap-loader-status' );
+	delete_option( 'dys-bootstrap-loader-use-minified' );
+	delete_option( 'dys-bootstrap-loader-parts' );
 }
 
 function activate_bootstrap_loader() {
 	/** add options to database **/
-	add_option('dys-bootstrap-loader-status', 'disabled', '', 'yes');
-	add_option('dys-bootstrap-loader-use-minified', 'yes', '', 'yes');
-	add_option('dys-bootstrap-loader-parts', 'both', '', 'yes');
+	add_option( 'dys-bootstrap-loader-status', 'disabled', '', 'yes' );
+	add_option( 'dys-bootstrap-loader-use-minified', 'yes', '', 'yes' );
+	add_option( 'dys-bootstrap-loader-parts', 'both', '', 'yes' );
 }
 
 function bootstrap_loader_configuration_interface() {
@@ -135,33 +135,33 @@ function bootstrap_loader_configuration_interface() {
 	$activeVersion = get_bootstrap_version();
 	$useMinified = get_bootstrap_minified();
 
-	if( isset($_GET['dys-set-bootstrap-loader-version'])) {
+	if( isset( $_GET['dys-set-bootstrap-loader-version'] ) ) {
 		// update active bootstrap version
 		$wantedVersion = $_GET['dys-set-bootstrap-loader-version'];
 
-		if (array_key_exists($wantedVersion, $bootstrap_versions)) {
-			if ($wantedVersion != $activeVersion) {
-				if (set_bootstrap_version($wantedVersion)) {
-					$message = __('Option successfully updated');
+		if ( array_key_exists( $wantedVersion, $bootstrap_versions ) ) {
+			if ( $wantedVersion != $activeVersion ) {
+				if ( set_bootstrap_version($wantedVersion) ) {
+					$message = __( 'Option successfully updated' );
 					$activeVersion = $wantedVersion;
 				} else {
-					$message = __('Updated failed !');
+					$message = __( 'Updated failed !' );
 				}
 			}
 		}
-		else { $message = __('The version you requested does not exists'); }
+		else { $message = __( 'The version you requested does not exists' ); }
 
-		if ( isset($_GET['dys-loader-use-minified']) && $_GET['dys-loader-use-minified'] == 'on') {
+		if ( isset($_GET['dys-loader-use-minified']) && $_GET['dys-loader-use-minified'] == 'on' ) {
 			$updateMinifiedOption = true;
 
-			if(enable_bootstrap_minified()) {
+			if( enable_bootstrap_minified() ) {
 				$useMinifiedUpdated = true;
 				$useMinified = true;
 			}
 			else { $useMinifiedUpdated = false; }
 		}
 		else {
-			if (disable_bootstrap_minified()) {
+			if ( disable_bootstrap_minified() ) {
 				$useMinifiedUpdated = true;
 				$useMinified = false;
 			}
@@ -169,15 +169,15 @@ function bootstrap_loader_configuration_interface() {
 		}
 
 		if ( $updateMinifiedOption && ! $useMinifiedUpdated ) {
-			$message .= '<p>'. __('Warning: could not set option "use minify"') . '</p>';
+			$message .= '<p>'. __( 'Warning: could not set option "use minify"' ) . '</p>';
 		}
 
-		if (isset($_GET['dys-loader-parts'])) {
+		if ( isset( $_GET['dys-loader-parts'] ) ) {
 			$loaderParts = $_GET['dys-loader-parts'];
 
-			if (($loaderParts == 'both' || $loaderParts == 'jsonly') || $loaderParts == 'cssonly') {
-				if (! set_bootstrap_parts($loaderParts)) {
-					$message .= '<p>' . __('Warning: Could not set option parts') . '</p>';
+			if ( ($loaderParts == 'both' || $loaderParts == 'jsonly') || $loaderParts == 'cssonly' ) {
+				if (! set_bootstrap_parts( $loaderParts ) ) {
+					$message .= '<p>' . __( 'Warning: Could not set option parts' ) . '</p>';
 				}
 			}
 		}
@@ -190,7 +190,7 @@ function bootstrap_loader_configuration_interface() {
 		'<input type="hidden" name="page" value="configure-dys-bootstrap">' .
 		'<select name="dys-set-bootstrap-loader-version">';
 
-	foreach ($bootstrap_versions as $version => $path) {
+	foreach ( $bootstrap_versions as $version => $path ) {
 		$configurationForm .= '<option value="'.$version.'" ';
 		if ($activeVersion == $version)
 			$configurationForm .= 'SELECTED';
@@ -199,19 +199,19 @@ function bootstrap_loader_configuration_interface() {
 
 	$configurationForm .= '<input type="checkbox" name="dys-loader-use-minified"';
 
-	if ($useMinified) {
+	if ( $useMinified ) {
 		$configurationForm .= 'CHECKED';
 	}
 
 	
 	$activeParts = get_bootstrap_parts();
-	if($activeParts == 'both') { $both = 'SELECTED'; }
+	if( $activeParts == 'both' ) { $both = 'SELECTED'; }
 	else { $both = ''; }
 
-	if($activeParts == 'jsonly') { $jsonly = 'SELECTED'; }
+	if( $activeParts == 'jsonly' ) { $jsonly = 'SELECTED'; }
 	else { $jsonly = ''; }
 
-	if($activeParts == 'cssonly') { $cssonly = 'SELECTED'; }
+	if( $activeParts == 'cssonly' ) { $cssonly = 'SELECTED'; }
 	else { $cssonly = ''; }
 
 	$configurationForm .= '></select><label for="dys-loader-use-minified">'.__('Use minified') . '</label>' .
@@ -223,7 +223,7 @@ function bootstrap_loader_configuration_interface() {
 				'<br><input type="submit">' .
 				'</form>';
 
-	echo $title.$message.$configurationForm;
+	echo $title . $message . $configurationForm;
 
 }
 
@@ -237,11 +237,11 @@ function admin_init_dys_bootstrap_loader() {
 			79);
 }
 
-add_action('wp_head','twitterbootstrap_header');
-add_action('wp_footer','twitterbootstrap_footer');
-add_action('admin_menu', 'admin_init_dys_bootstrap_loader');
+add_action( 'wp_head','twitterbootstrap_header' );
+add_action( 'wp_footer','twitterbootstrap_footer' );
+add_action( 'admin_menu', 'admin_init_dys_bootstrap_loader' );
 
-register_activation_hook(__FILE__, 'activate_bootstrap_loader');
-register_deactivation_hook(__FILE__, 'deactivate_bootstrap_loader');
+register_activation_hook( __FILE__, 'activate_bootstrap_loader' );
+register_deactivation_hook( __FILE__, 'deactivate_bootstrap_loader' );
 
 ?>
